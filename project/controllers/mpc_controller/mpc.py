feb72f8e696999ac5c5428951500d4bbe6fa859f
Amin Mahjoub <mahjoub@usc.edu>
2023-03-29 17:50:59 -0700

    added mpc with scipy but it is not working will try gekko

--- project/controllers/mpc_controller/mpc.py
+++ project/controllers/mpc_controller/mpc.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cvxpy as cp
 from scipy.linalg import block_diag
-
-
+from scipy.optimize import NonlinearConstraint, LinearConstraint, minimize, Bounds
+from math import factorial
 
 def imageSubplot(data:list, title:list, showFig:bool=True):
         plt.figure()
@@ -15,120 +15,109 @@ def imageSubplot(data:list, title:list, showFig:bool=True):
             plt.show()
 
 class MPC(object):
-    def __init__(self, speed, H, pos:tuple) -> None:
+    def __init__(self, speed, H, pos, dt, goalX, goalY) -> None:
         self.x_init = pos[0]
         self.y_init = pos[1]
+        self.theta_init = pos[2]
         self.time_steps = H
-        self.number_of_states = H
-        self.number_of_controls = H - 1
-        self.V = speed
+        self.number_of_states =  3 * (H+1)
+        self.number_of_controls = H
+        self.num_decisions = self.number_of_states + self.number_of_controls
+        self.X0 = np.zeros(self.num_decisions)
+        self.X0[0] = self.x_init
+        self.X0[H+1] = self.y_init
+        self.X0[2*(H+1)] = self.theta_init
+        self.H = H
+        self.goalX = goalX
+        self.goalY = goalY
+        self.dt = dt
+        self.v = speed
+        self.c = self.constraints()
+        self.N = self.X0.shape[0]
+        self.Window = int((self.N-3)/4)
 
-    def constraints():
+    def setBounds(self):
         pass
-    def cost():
-        pass
-    def run(self):
-        pass
-     
-     
-def mpc():
-
-    ## TODO: ADD dynamics constraints and cost function
-    '''
-        constraints:
-            1. cannot not be outside the road
-                30 + (75 - 10.75) < x_pose < 30 + (75 + 10.75)
-                30 + (75 - 10.75) < y_pose < 30 + (75 + 10.75)
-            2. dynamics constraints 
-                X - vsin(theta) = 0
-                Y - viscose(theta) = 0
-                Theta - omega = 0
-                V - a = 0 (constant speed)
-            3. control constraints (tbd)
-                abs(omega) < pi /2 
-                
-        cost function:
-            cost = sum(q * (x_pose^2 + y_pose^2) + r * omega^2) + q_f * (x_pose^2 + y_pose^2)
-            
-            from mpc example:
-                f1 = sum((x(1:H+1) - goalX).^2) + sum((x(2*H+2:3*H+2) - goalY).^2);
-        X:
-            x_pose.Size = (H, 1) where H is horizon  
-            y_pose.Size = (H, 1) where H is horizon 
-            theta.Size = (H, 1) where H is horizon
-
-            X = [x_pose; 
-                y_pose; 
-                theta]
-    '''
-    a = 1
-    b = 1
-    q =1
-    q_f =1 
-    r = 50
-    x_init = 1
-    y_init = 1
-    time_steps = 3
-    number_of_states = 5
-    number_of_controls = 4
-    dt = 0.5
-    v = 0.1 # constant speed
-    AeqX  = -1 * np.eye(time_steps, time_steps + 1)
-    tempX = -dt * np.eye(time_steps)
-    AeqX = np.concatenate((AeqX, tempX), axis=1)
-
-    AeqY = -1 * np.eye(time_steps, time_steps + 1)
-    tempY = -dt * np.eye(time_steps)
-    AeqY = np.concatenate((AeqY, tempY), axis=1)
-
-    BeqY = np.zeros((time_steps))
-    BeqX = np.zeros((time_steps))
-
-    for i in range(time_steps):
-        AeqY[i][i+1] = 1
-        AeqX[i][i+1] = 1
-
-    
-    Aeq = block_diag(AeqX, AeqY)
-    Beq = np.concatenate((BeqX, BeqY), axis=0)
-
-    print(Aeq)
-    print(Aeq.shape)
-    print(Aeq.shape)
-    print(BeqX.shape)
-    
-    x = cp.Variable((2 * number_of_states) + number_of_controls)
-
-    constraint = [
-        Aeq @ x == Beq, 
-        x[0] - x_init == 0, 
-        x[number_of_states] - y_init == 0,
-        x[:number_of_states] >= 30 + (75 - 10.75),
-        x[:number_of_states] <= 30 + (75 + 10.75),
-        x[number_of_states: 2 * number_of_states] >= 30 + (75 - 10.75),
-        x[number_of_states: 2 * number_of_states] <= 30 + (75 + 10.75),
-        x[2 * number_of_states:] <= np.pi / 2,
-        x[2 * number_of_states:] >= -np.pi / 2,
-    ]
-    
-    # cost = 0
-    # for i in range(time_steps):
-    #     print(i + time_steps - 2)
-    #     cost += q * x[i] ** 2 + r * x[i + time_steps - 1] ** 2
+  
+    def xPosConstraint(self, x,i, ceq):
+        ceq[i] = x[i+1] - x[i] - self.dt * self.v * np.cos(x[2*(self.Window+1) + i]) 
+        ceq[self.H + i] = x[(self.Window +1) + i + 1] - x[self.Window + 1 + i] - self.dt * self.v * np.sin(x[2*(self.Window+1) + i])
+        ceq[2*self.H + i] = (x[2*(self.Window +1) + i + 1] - x[2*(self.Window +1) + i] - self.dt * x[3*(self.Window+1) + i])
         
-    # cost += q_f * x[38] ** 2
+        x_initial = self.X0[0] - self.x_init * 0.5
+        y_initial = self.X0[self.Window+1] - self.y_init * 0.5
+        theta_intial = self.X0[2*self.Window +3] - self.theta_init * 0.5
+        
+        #x_initial, y_initial, theta_intial)
 
-    # objective = cp.Minimize(cost)
-    # prob = cp.Problem(objective, constraint)
-    # prob.solve()
+        ceq = np.vstack((ceq, x_initial, y_initial, theta_intial))
+        ceq = ceq.flatten()
+        # print(ceq.shape)
+        #print(ceq)
+        return ceq
+    
+    def constraintPos(self, x,):
+        ceq = np.zeros((3*self.H,1))
+        for i in range(self.time_steps):
+            ceq[i] = x[i+1] - x[i] - self.dt * self.v * np.cos(x[2*(self.Window+1) + i]) 
+            ceq[self.H + i] = x[(self.Window +1) + i + 1] - x[self.Window + 1 + i] - self.dt * self.v * np.sin(x[2*(self.Window+1) + i])
+            ceq[2*self.H + i] = (x[2*(self.Window +1) + i + 1] - x[2*(self.Window +1) + i] - self.dt * x[3*(self.Window+1) + i])
+            
+        x_initial = self.X0[0] - self.x_init
+        y_initial = self.X0[self.Window+1] - self.y_init
+        theta_intial = self.X0[2*self.Window +3] - self.theta_init
+        
+        #x_initial, y_initial, theta_intial)
 
-    # print("\nThe optimal value is", prob.value)
-    # print("A solution x is")
+        ceq = np.vstack((ceq, x_initial, y_initial, theta_intial))
+        ceq = ceq.flatten()
 
-    # x_final = x.value[:20]
-    # u_final = x.value[20:39]
+        return ceq
 
-    # imageSubplot([x_final, u_final], ["MPC state", "MPC control"])
-    # print(Aeq)
+    def constraints(self):
+        N = self.X0.shape[0]
+        Window = int((N-3)/4)
 
-mpc()
+        x_init = lambda x : x[0] - self.x_init
+        y_init = lambda x : x[Window+1] - self.y_init
+        theta_init = lambda x : x[2*Window + 3] - self.theta_init
+       
+        c = []
+        scipy_c = []
+       
+        #scipy_c.append(NonlinearConstraint(self.xPosConstraint, 0, 0))
+        scipy_c.append({'type': 'eq', 'fun': self.xPosConstraint})
+
+        #cons_per_i = [{'type': 'eq', 'fun': self.xPosConstraint, 'args':(i,)} for i in range(self.time_steps)]
+        ceq = np.zeros((3*self.H,1))
+        cons_per_i = [{'type':'eq', 'fun': lambda z, i=i: self.xPosConstraint(z, i, ceq)} for i in range(self.time_steps)][-1]
+        print(cons_per_i)
+        #cons_per_i = [{'type':'eq', 'fun': self.constraintPos}]
+        return cons_per_i
+    
+    def cost(self, x):
+        c = np.sum((x[:self.H] - self.goalX) ** 2   + (x[self.H+1:2*self.H +1] - self.goalY) ** 2)
+        return c
+    
+    def run(self):
+        # prob = cp.Problem(self.objective, self.c)
+        # prob.solve()
+        # print(self.X0.value)
+        #[-100*ones(H+1, 1); -100*ones(H+1, 1);-100*ones(H+1, 1); -wMax*ones(H, 1)];'
+      
+        bnds = -100* np.ones((self.time_steps+1,1))
+        temp =  -100* np.ones((self.time_steps+1,1))
+        temp2 =  -100* np.ones((self.time_steps+1,1))
+        temp1 = -1.1* np.ones((self.time_steps,1))
+        lbnds = np.vstack((bnds, temp2, temp, temp1))
+      
+        ubnds = -lbnds
+        
+        bounds = []
+        for i in range (self.X0.shape[0]):
+            bounds.append((lbnds[i], ubnds[i]))
+        req = minimize(self.cost, self.X0, method='SLSQP', constraints=self.c, options={'disp': True}, bounds=bounds)
+        print(req.x)
+
+mpc_obj = MPC(0.1, 10, [-4, -4, 0], 0.5, goalX=10, goalY=10)
+mpc_obj.run()
